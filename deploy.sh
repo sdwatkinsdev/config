776bbaa513c01a75d1e77c61b7185eae0b860922
Stephen Watkins <sdwatkinsdev@gmail.com>
2018-09-26 12:54:37 -0400

    much better deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,24 @@
 # supposedly this reliably returns the location of the current script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+find_or_link () {
+    local filename="$1"
+    local source_dir="$2"
+    local target_dir="$3"
+
+    local source="$source_dir/$filename"
+    local target="$target_dir/$filename"
+    if [ -e "$target" ]
+    then
+        padded=$(printf '%-20s' $filename)
+        echo "$padded already exists at $target"
+    else
+        ln "$source" "$target_dir"
+        padded=$(printf '%-18s' $filename)
+        echo "$padded +++ link created at $target"
+    fi
+}
+
 
 echo "-----------------"
 echo "Home files"
@@ -14,17 +32,7 @@ echo "-----------------"
 HOME_FILES=(".gitconfig" ".vimrc" ".pryrc")
 for file in "${HOME_FILES[@]}"
 do
-    source=$DIR/$file
-    target=~/$file
-    if [ -e "$target" ]
-    then
-        padded=$(printf '%-14s' $file)
-        echo "$padded already exists at $target"
-    else
-        ln $source ~
-        padded=$(printf '%-12s' $file)
-        echo "$padded + link created at $target"
-    fi
+    find_or_link $file $DIR ~
 done
 
 echo
@@ -35,26 +43,13 @@ echo "-----------------"
 VSCODE_SOURCE_DIR=$DIR/.vscode
 VSCODE_SOURCES=($(ls $VSCODE_SOURCE_DIR))
 
-
-%APPDATA%\Code\User\settings.json
 case $(uname) in
-    Linux*|Darwin*)  VSCODE_TARGET_DIR=~/Library/Application\ Support/Code/User;;
+    Linux*|Darwin*)  VSCODE_TARGET_DIR="$HOME/Library/Application Support/Code/User";;
     MINGW*|CYGW*)    VSCODE_TARGET_DIR=$APPDATA/Code/User;;
-    *) exit 1
+    *) echo "Failed to detect VS Code settings dir from uname '$(uname)'"; exit 1
 esac
-
 
 for file in "${VSCODE_SOURCES[@]}"
 do
-    source=$VSCODE_SOURCE_DIR/$file
-    target=$VSCODE_TARGET_DIR/$file
-    if [ -e "$target" ]
-    then
-        padded=$(printf '%-14s' $file)
-        echo "$padded already exists at $target"
-    else
-        ln $source ~
-        padded=$(printf '%-12s' $file)
-        echo "$padded + link created at $target"
-    fi
+    find_or_link $file "$VSCODE_SOURCE_DIR" "$VSCODE_TARGET_DIR"
 done
